7877677dba0365d6c7a2c5cc496e2f26d14853ce
Alasdair Orr <158096722+49ersMcAfree@users.noreply.github.com>
2024-02-13 16:37:24 -0600

    Update VinylRecordProduct.java

--- K_Homework_3/src/invmanage/VinylRecordProduct.java
+++ K_Homework_3/src/invmanage/VinylRecordProduct.java
@@ -1,1 +1,12 @@
+package invmanage;
+//  VinylRecordProduct class
+class VinylRecordProduct extends MediaProduct {
+    public VinylRecordProduct(String title, double price, int year, Genre genre) {
+        super(title, price, year, genre);
+    }
 
+    // Copy constructor
+    public VinylRecordProduct(CDRecordProduct other) {
+        super(other);
+    }
+}
