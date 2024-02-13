c9766c60045e19341ddff5faa6b1cbc794fabd23
CWill22 <147526756+CWill22@users.noreply.github.com>
2024-02-13 16:28:33 -0600

    started initializeStock

--- K_Homework_3/src/invmanage/StockManagerSingleton.java
+++ K_Homework_3/src/invmanage/StockManagerSingleton.java
@@ -1,5 +1,25 @@
 package invmanage;
 
-public class StockManagerSingleton {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class StockManagerSingleton {
+		
+	public boolean initializeStock() {
+		try {
+			
+		    // Read the Data from the csv file "inventory.csv" line by line
+			final String inventoryFilePath = "inventory.csv";
+		
+			Scanner fileIn = new Scanner(new FileInputStream(inventoryFilePath));
+			
+			
+			}catch(FileNotFoundException e)
+			{
+				e.printStackTrace();
+				return false;
+			}
+		    return true;
+	}
 }
