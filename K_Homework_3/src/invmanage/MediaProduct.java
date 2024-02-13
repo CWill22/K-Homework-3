42b671e0a94503b7192ce491c2067729231898d2
Alasdair Orr <158096722+49ersMcAfree@users.noreply.github.com>
2024-02-13 16:04:49 -0600

    Update MediaProduct.java

--- K_Homework_3/src/invmanage/MediaProduct.java
+++ K_Homework_3/src/invmanage/MediaProduct.java
@@ -1,5 +1,20 @@
 package invmanage;
 
 public class MediaProduct {
+  // Genre enum
+enum Genre {
+    ROCK,
+    POP,
+    CLASSICAL,
+    JAZZ
+}
+
+// MediaProduct class
+class MediaProduct {
+    protected String title;
+    protected double price;
+    protected int year;
+    protected Genre genre;
+
 
 }
