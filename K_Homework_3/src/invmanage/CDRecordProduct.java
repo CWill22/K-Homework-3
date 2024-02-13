a790cdfdf4af0094200db6badeff68b9a5f54520
Alasdair Orr <158096722+49ersMcAfree@users.noreply.github.com>
2024-02-13 16:29:08 -0600

    CDRecordProduct for containing the constructor and superkeyword to intialize

--- K_Homework_3/src/invmanage/CDRecordProduct.java
+++ K_Homework_3/src/invmanage/CDRecordProduct.java
@@ -1,1 +1,13 @@
+package invmanage;
 
+// CDRecordProduct class
+class CDRecordProduct extends MediaProduct {
+    public CDRecordProduct(String title, double price, int year, Genre genre) {
+        super(title, price, year, genre);
+    }
+
+    // Copy constructor
+    public CDRecordProduct(CDRecordProduct other) {
+        super(other);
+    }
+}
