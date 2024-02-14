f21a61277a95ba7e008e995e2d6f925521151a22
Alasdair Orr <158096722+49ersMcAfree@users.noreply.github.com>
2024-02-13 19:32:55 -0600

    Update TapeRecordProduct.java Copy Constructor

--- K_Homework_3/src/invmanage/TapeRecordProduct.java
+++ K_Homework_3/src/invmanage/TapeRecordProduct.java
@@ -1,1 +1,12 @@
+package invmanage;
+//  TapeRecordProduct class
+class TapeRecordProduct extends MediaProduct {
+    public TapeRecordProduct(String title, double price, int year, Genre genre) {
+        super(title, price, year, genre);
+    }
 
+    // Copy constructor
+    public TapeRecordProduct(CDRecordProduct product) {
+        super(product.title, product.price, product.year, product.genre);
+    }
+}
