1f67334afb1aa9d221c473f30a1647951b7548ce
CWill22 <147526756+CWill22@users.noreply.github.com>
2024-02-15 16:49:05 -0600

    start main

--- K_Homework_3/src/invmanage/Main.java
+++ K_Homework_3/src/invmanage/Main.java
@@ -4,7 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// Create a new StockManagerSingleton instance
+		StockManagerSingleton stockManager = new StockManagerSingleton();
+	    
+		stockManager.initializeStock();
+	    // Add a new VinylRecordProduct to the stock
+		        stockManager.addItem(new VinylRecordProduct("The Dark Side of the Moon", 29.99, 1973, Genre.ROCK));
+		      
 
 	}
 
+	
 }
