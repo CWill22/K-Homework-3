bece464991968a77d540010cccf1b0d2d55dfecb
CWill22 <147526756+CWill22@users.noreply.github.com>
2024-02-16 16:16:42 -0600

    update comments

--- K_Homework_3/src/invmanage/VinylRecordProduct.java
+++ K_Homework_3/src/invmanage/VinylRecordProduct.java
@@ -1,7 +1,6 @@
 package invmanage;
 //  VinylRecordProduct class
 class VinylRecordProduct extends MediaProduct {
-    // vinyl array 
     public VinylRecordProduct(String title, double price, int year, Genre genre) {
         super(title, price, year, genre);
     }
